ed8e1faec80a3fd30cc9c7c6c5775deb0e7bfa03
Broc Nickodemus <brocdnickodemus@gmail.com>
2024-02-12 15:25:34 -0800

    Update bitwise.c

--- bitwise.c
+++ bitwise.c
@@ -51,7 +51,7 @@ void bitwise(void)
 	/*
 	typedef unsigned char uint8_t;
 	typedef unsigned short uint16_t;
-	typedef unsigned long uint32_t;
+	typedef unsigned int uint32_t;
 	typedef unsigned long long uint64_t;
 	*/
 
